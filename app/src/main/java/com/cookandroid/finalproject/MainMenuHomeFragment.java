efc6b58afe5a9a9e387b9d36b45e860de8a11205
katnon <gunho000316@gmail.com>
2024-06-18 22:43:20 +0900

    last commit

--- app/src/main/java/com/cookandroid/finalproject/MainMenuHomeFragment.java
+++ app/src/main/java/com/cookandroid/finalproject/MainMenuHomeFragment.java
@@ -194,6 +194,7 @@ public class MainMenuHomeFragment extends Fragment {
                             }
                         }, year, month, day);
                 datePickerDialog.show();
+                getView();
             }
         });
 
